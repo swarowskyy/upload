5e3b48c5da2b7a89debde3243fd11f346127b648
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-05-25 17:01:00 -0300

    a

--- fr4ontend/src/main.ts
+++ fr4ontend/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { Galeria } from './app/app';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Galeria, appConfig)
   .catch((err) => console.error(err));
