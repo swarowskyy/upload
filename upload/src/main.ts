a7fa08328c711bd5458074904ab43b980c8297d0
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-05-25 16:44:00 -0300

    a

--- upload/src/main.ts
+++ upload/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+ import { App } from './app/app';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //NÃO ESQUECER
